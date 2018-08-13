519bb926535ebb84d9ec56610fb287792496af28
Sheng Zheng <szheng@e1z3r7p9.42.us.org>
2018-08-12 19:47:54 -0700

    modified testmain and add multiple files to test compatibility

--- testmain.c
+++ testmain.c
@@ -1,22 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   testmain.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: szheng <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/12 19:43:20 by szheng            #+#    #+#             */
+/*   Updated: 2018/08/12 19:43:22 by szheng           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
 int   main(int ac, char **av)
 {
-  char *line;
-  int fd;
+  char  *line;
+  int   fd1;
+  int   fd2;
 
-  fd = open(av[1], O_RDONLY);
-  get_next_line(fd, &line);
+  fd1 = open(av[1], O_RDONLY);
+  fd2 = open(av[2], O_RDONLY);
+  get_next_line(fd1, &line);
   printf("%s\n", line);
-	get_next_line(fd, &line);
+  get_next_line(fd1, &line);
   printf("%s\n", line);
-	get_next_line(fd, &line);
-	printf("%s\n", line);
-	get_next_line(fd, &line);
-	printf("%s\n", line);
-	get_next_line(fd, &line);
-	printf("%s\n", line);
-	get_next_line(fd, &line);
-	printf("%s\n", line);
+  get_next_line(fd2, &line);
+  printf("%s\n", line);
+  get_next_line(fd2, &line);
+  printf("%s\n", line);
+  get_next_line(fd1, &line);
+  printf("%s\n", line);
+  get_next_line(fd2, &line);
+  printf("%s\n", line);
+  get_next_line(fd1, &line);
+  printf("%s\n", line);
+  get_next_line(fd2, &line);
+  printf("%s\n", line);
   return (0);
 }
